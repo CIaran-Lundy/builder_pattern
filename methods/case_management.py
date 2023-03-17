c0c3d7117d89bcc0f9c73b619c8d7e0984d9be55
CIaran-Lundy <60901278+CIaran-Lundy@users.noreply.github.com>
2023-03-17 13:42:47 +0000

    Create case_management.py

---
+++ methods/case_management.py
@@ -0,0 +1,8 @@
+# a module for holding all methods that manage the case of strings
+
+def upper(text: str) -> str:
+    return text.upper()
+
+
+def lower(text: str) -> str:
+    return text.lower()
